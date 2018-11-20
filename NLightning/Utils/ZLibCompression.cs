df26e98e47be1f03382ccb76c34c2426b3f5e8a1
domsch <nlightning@protonmail.com>
2018-11-20 11:02:56 +0100

    Gossip Query: fixed zLib compression issues

--- NLightning/Utils/ZLibCompression.cs
+++ NLightning/Utils/ZLibCompression.cs
@@ -1,27 +1,32 @@
 using System.IO;
 using System.IO.Compression;
+using NLightning.Utils.Extensions;
 
 namespace NLightning.Utils
 {
     public class ZLibCompression
     {
+        private static readonly byte[] ZlibDefaultCompressionHeader = "789C".HexToByteArray();
+        
         public byte[] Compress(byte[] data)
         {
             using (MemoryStream inputStream = new MemoryStream(data))
             using (MemoryStream outputStream = new MemoryStream())
             {
+                outputStream.Write(ZlibDefaultCompressionHeader);
+                
                 using (DeflateStream deflateStream = new DeflateStream(outputStream, CompressionMode.Compress))
                 {
                     inputStream.CopyTo(deflateStream);
                 }
-                
+
                 return outputStream.ToArray();
             }
         }
 
         public byte[] Decompress(byte[] data)
         {
-            using (MemoryStream inputStream = new MemoryStream(data))
+            using (MemoryStream inputStream = new MemoryStream(data, 2, data.Length - 2))
             using (MemoryStream outputStream = new MemoryStream())
             {
                 using (DeflateStream decompressionStream = new DeflateStream(inputStream, CompressionMode.Decompress))
